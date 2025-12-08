10d5eabcbe03de28e5f7311c2dfe121b520d7dc8
nicoloprog <nicosubywrx@gmail.com>
2025-12-08 09:49:30 -0500

    fixing error

--- lib/shopify/index.ts
+++ lib/shopify/index.ts
@@ -486,11 +486,11 @@ export async function revalidate(req: NextRequest): Promise<NextResponse> {
   }
 
   if (isCollectionUpdate) {
-    revalidateTag(TAGS.collections);
+    revalidateTag(TAGS.products, "max");
   }
 
   if (isProductUpdate) {
-    revalidateTag(TAGS.products);
+    revalidateTag(TAGS.products, "max");
   }
 
   return NextResponse.json({ status: 200, revalidated: true, now: Date.now() });
