8e4f463be866195ed82673ba9a2c2034936e93d7
nicoloprog <nicosubywrx@gmail.com>
2026-01-27 07:58:13 -0500

    new thing 6

--- components/featuredimage.tsx
+++ components/featuredimage.tsx
@@ -5,7 +5,7 @@ export function FeaturedImage() {
   return (
     <div className="absolute w-full h-[600px] md:h-[800px]">
       {/* Main featured image - absolutely positionable */}
-      <div className="absolute top-[146.25%] left-[-65%] md:top-[182.65%] md:left-20 w-[60rem] md:w-[120rem] z-50">
+      <div className="absolute top-[150.25%] left-[-65%] md:top-[182.65%] md:left-20 w-[60rem] md:w-[120rem] z-50">
         <div className="aspect-[3/4] md:aspect-[4/3]">
           <img
             src="/science.png"
